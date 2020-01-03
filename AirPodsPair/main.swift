b71592a2be35593dc6b1df03a361fc4ab2bc8b30
JoeManto <joemanto123@gmail.com>
2020-01-03 12:24:15 -0500

    init

--- AirPodsPair/main.swift
+++ AirPodsPair/main.swift
@@ -7,6 +7,56 @@
 //
 
 import Foundation
+import IOBluetooth
 
-print("Hello, World!")
+
+let deviceName = "Joe’s AirPods"
+let pairedDevices = IOBluetoothDevice.pairedDevices()!
+
+func search(for deviceName:String) -> IOBluetoothDevice? {
+    for i in 0...pairedDevices.count-1 {
+        print("Paired Device \(i)")
+        let device = pairedDevices[i] as? IOBluetoothDevice
+        
+        if(device?.name == deviceName){
+            print("Found Device \(device?.name ?? "Device has no name")")
+            return device
+        }
+    }
+    return nil
+}
+
+func pair(device:IOBluetoothDevice!) -> Bool {
+    
+    //device is already connected
+    if(device.isConnected())
+    {
+        return true
+    }
+    
+    //attempt to pair device
+    if(device.openConnection() == KERN_SUCCESS){
+        print("connectioned to device")
+        return true
+    }
+    
+    return false
+   
+}
+
+var tarDevice:IOBluetoothDevice? = search(for:deviceName)
+
+if(tarDevice != nil){
+    let res = pair(device: tarDevice)
+}
+
+
+
+
+
+
+
+
+
+
 
